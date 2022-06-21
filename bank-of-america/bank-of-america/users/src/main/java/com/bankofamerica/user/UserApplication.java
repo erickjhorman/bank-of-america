38afb097507e16ff3e16ebf0b9fba10329d15f9f
erick <erickjhorman.romero@globant.com>
2022-06-21 17:45:04 -0500

    create user  package

---
+++ bank-of-america/bank-of-america/users/src/main/java/com/bankofamerica/user/UserApplication.java
@@ -0,0 +1,11 @@
+package com.bankofamerica.user;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class UserApplication {
+    public static void main(String[] args) {
+
+    }
+}
